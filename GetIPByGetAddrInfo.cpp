f0243cc41988fda369fdce68c4e41a1402d8382a
Kami <Kami@DESKTOP-MHA0S9Q>
2021-06-30 12:58:38 +0800

    添加项目文件。

---
+++ GetIPByGetAddrInfo.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#pragma comment(lib,"ws2_32.lib")
+
+void GetMyIP(char* hostname) {
+	struct addrinfo phint;
+	struct addrinfo* pResult;
+	memset(&phint, 0, sizeof(struct addrinfo));
+
+	phint.ai_family = AF_INET;
+	phint.ai_flags = AI_PASSIVE;
+	phint.ai_protocol = IPPROTO_IP;
+
+	int ret = getaddrinfo(hostname, NULL, &phint, &pResult);
+	for (addrinfo* tmp = pResult; tmp != nullptr; tmp = tmp->ai_next) {
+		sockaddr_in* addr = (sockaddr_in*)tmp->ai_addr;
+		char str[INET_ADDRSTRLEN];
+		std::cout << inet_ntop(AF_INET, (char*)&addr->sin_addr, str, sizeof(str)) << std::endl;
+	}
+}
+
+int main(int argc, char* argv[])
+{
+	WSAData wsData;
+	WSAStartup(MAKEWORD(2, 2), &wsData);
+
+	char hostname[256];
+
+	if (argc < 2) gethostname(hostname, sizeof(hostname));
+	else memcpy(hostname, argv[1], sizeof(hostname));
+
+	GetMyIP(hostname);
+
+	WSACleanup();
+}
